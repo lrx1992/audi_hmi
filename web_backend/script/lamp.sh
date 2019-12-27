feeda6e4f88ca786f9ebfe4a09b1c44abf53f1d6
ruixin.liu <ruixin.liu@horizon.ai>
2019-12-27 16:48:34 +0800

    add lamp.sh

--- web_backend/script/lamp.sh
+++ web_backend/script/lamp.sh
@@ -1,3 +1,29 @@
-ps -fe | grep "audi_lamp" | awk '{print $2}' |xargs kill -9
-nohup python /home/pi/src/rpi-ws281x-python/examples/audi_lamp.py -m 2 >/dev/null &
 
+#ssh -t hadoop@10.101.35.209 "sudo touch /etc/b.txt"
+
+#ps -fe | grep "audi_lamp" | awk '{print $2}' |xargs kill -9
+#nohup python /home/pi/src/rpi-ws281x-python/examples/audi_lamp.py -m 2 >/dev/null &
+
+
+
+stop() {
+  ssh -t pi@192.168.1.65 "ps -fe | grep "audi_lamp" | awk '{print $2}' |xargs kill -9 ; nohup python /home/pi/src/rpi-ws281x-python/examples/audi_lamp.py -m 0 >/dev/null &"
+  ssh -t pi@192.168.1.66 "ps -fe | grep "audi_lamp" | awk '{print $2}' |xargs kill -9 ; nohup python /home/pi/src/rpi-ws281x-python/examples/audi_lamp.py -m 0 >/dev/null &"
+}
+
+start() {
+  ssh -t pi@192.168.1.65 "ps -fe | grep "audi_lamp" | awk '{print $2}' |xargs kill -9 ; nohup python /home/pi/src/rpi-ws281x-python/examples/audi_lamp.py -m 2 >/dev/null &"
+  ssh -t pi@192.168.1.66 "ps -fe | grep "audi_lamp" | awk '{print $2}' |xargs kill -9 ; nohup python /home/pi/src/rpi-ws281x-python/examples/audi_lamp.py -m 2 >/dev/null &"  
+}
+
+case $1 in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  *)
+    start
+    ;;
+esac
