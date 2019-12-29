73057806d568c865208a2c1183412b82fea9856f
lrx1992 <709875298@qq.com>
2019-12-30 07:53:58 +0800

    add client

--- web_backend/script/client.py
+++ web_backend/script/client.py
@@ -3,6 +3,6 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 while 1:
     data = input('input:')
+    data = "data:"+data
     s.sendto(data.encode(),("192.168.1.66",9090))
-
-
+    s.sendto(data.encode(),("192.168.1.65",9090))
